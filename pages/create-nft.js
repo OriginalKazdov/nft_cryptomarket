d578fa6fe6c4a2e0c41ac1f1a08f600140471b4c
OriginalKazdov <juandovzak@gmail.com>
2024-04-12 01:41:44 -0300

    Agregando routings

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React from 'react';
 
-function CreateNFT() {
-  return <div>CreateNFT</div>;
-}
+const CreateNFT = () => <div>CreateNFT</div>;
 
 export default CreateNFT;
