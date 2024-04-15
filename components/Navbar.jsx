fccfae7870238601e548c4a31583fbfcac73f728
OriginalKazdov <juandovzak@gmail.com>
2024-04-15 00:10:01 -0300

    Update to navbar 2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,9 +9,17 @@ import images from '../assets';
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
-  console.log({ theme });
   return (
-    <div>test</div>
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Cryptoket</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
